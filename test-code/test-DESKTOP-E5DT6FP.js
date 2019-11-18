05219ebc03fe5190df6f73eaa7d0c2ca960e41b4
Jason Yu <jasonyu1994@gmail.com>
2019-11-18 09:41:18 -0800

    test

--- test-code/test-DESKTOP-E5DT6FP.js
+++ test-code/test-DESKTOP-E5DT6FP.js
@@ -94,3 +94,20 @@ fn(5,3);
 fn(4,5);
 fn(2,7);
 */
+
+/*
+function twoSum(arr, S) {
+  var sums = [];
+  var hashTable = {};
+  for (var i = 0; i < arr.length; i++) {
+    var sumMinusElement = S - arr[i];
+    if (hashTable[sumMinusElement.toString()] !== undefined) {
+      sums.push([arr[i], sumMinusElement]);
+    }
+    hashTable[arr[i].toString()] = arr[i];
+  }
+  return sums;
+}
+
+console.log(twoSum([3, 5, 2, -4, 8, 11], 7).toString());
+*/ 
